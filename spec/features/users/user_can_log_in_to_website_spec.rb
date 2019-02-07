a32cec043c26f9e9311c3db64664d80c19a52241
vjp888 <rellik888@gmail.com>
2019-02-07 13:04:07 -0700

    Adds test for user login

---
+++ spec/features/users/user_can_log_in_to_website_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'as a visitor', type: :feature do
+
+  it 'should let a user log in with credentials' do
+    User.create(full_name: "Steve Jobs", email: "1234@4321.com", password: "password")
+
+    visit(sessions_path)
+
+    fill_in 'Email', with: '1234@4321.com'
+    fill_in 'password', with: 'password'
+    click_button 'Login'
+
+    expect(current_path).to eq(session_path(:id))
+  end
+end
