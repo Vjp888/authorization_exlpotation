b42fe452dddc50c2dbcd6578e821fe5ac8729fa7
vjp888 <rellik888@gmail.com>
2019-02-07 12:47:33 -0700

    Adds authentication class method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   validates_presence_of :full_name
   validates_presence_of :email
   validates_presence_of :password
+
+
+  def self.authenticate(email, password)
+    where(email: email, password: password).first
+    # binding.pry
+  end
 end
