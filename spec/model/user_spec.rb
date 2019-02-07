180a3b4c54f61f34a5604758b3123cfdc7b38e8b
vjp888 <rellik888@gmail.com>
2019-02-07 12:34:54 -0700

    adds test for authentication class method

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :password}
   end
 
+  describe 'class methods' do
+    describe '.authenticate' do
+      it 'should return a user when correct email and password are sent' do
+        steve = User.create(full_name: "Steve Jobs", email: "1234@4321.com", password: "password")
+        jacob = User.create(full_name: "Jacobs Fletcher", email: "Dun@nam", password: "12345")
+
+        good_cred = User.authenticate("1234@4321.com", "password")
+        bad_cred = User.authenticate("1234@4321.com", "pssword")
+
+        expect(good_cred).to eq(steve)
+        expect(bad_cred).to eq(nil)
+        expect(good_cred).to_not eq(jacob)
+      end
+    end
+  end
 end
